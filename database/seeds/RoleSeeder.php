c9f1b5d5a77a404b9d1e6dabc9fff2af568a2cab
JoelNzomo <joel.nzomo@obulexsolutions.com>
2016-11-04 10:08:41 +0300

    seeder

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -11,16 +11,16 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(CustomerTypesSeeder::class);
-//         $this->call(MasterfileSeeder::class);
-//         $this->call(UserLoginSeeder::class);
-//         $this->call(RequestTypesSeeder::class);
-//         $this->call(transaction_categories_seeder::class);
-//         $this->call(transaction_types_seeder::class);
+        $this->call(CustomerTypesSeeder::class);
+         $this->call(MasterfileSeeder::class);
+         $this->call(UserLoginSeeder::class);
+         $this->call(RequestTypesSeeder::class);
+         $this->call(transaction_categories_seeder::class);
+         $this->call(transaction_types_seeder::class);
         $this->call(RoleSeeder::class);
-//         $this->call(AddressTypeSeeder::class);
-//         $this->call(CountySeeder::class);
-//         $this->call(RouteSeeder::class);
-//         $this->call(MenuSeeder::class);
+         $this->call(AddressTypeSeeder::class);
+         $this->call(CountySeeder::class);
+         $this->call(RouteSeeder::class);
+         $this->call(MenuSeeder::class);
     }
 }
