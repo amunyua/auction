a1d313bad66cf42b041237d388b848923d90de00
JoelNzomo <joel.nzomo@obulexsolutions.com>
2016-11-09 14:38:12 +0300

    dslfjlsd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(CustomerTypesSeeder::class);
          $this->call(MasterfileSeeder::class);
+         $this->call(RoleSeeder::class);
          $this->call(UserLoginSeeder::class);
          $this->call(RequestTypesSeeder::class);
          $this->call(transaction_categories_seeder::class);
          $this->call(transaction_types_seeder::class);
-         $this->call(RoleSeeder::class);
          $this->call(AddressTypeSeeder::class);
          $this->call(CountySeeder::class);
          $this->call(RouteSeeder::class);
